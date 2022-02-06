192d4a6fb4c5fadfcb3b342d737a73178d79a71a
George Spyrou <gspyrou@umich.edu>
2022-02-06 21:03:03 +0000

    add func to identify full capitalized video titles

--- eda.py
+++ eda.py
@@ -72,3 +72,21 @@ plot_trending_date_change_for_col(
     col='target'
     )
 
+youtube_df_train['title'][0]
+
+
+def is_title_full_capital(video_title: str, threshold: float = 0.7) -> bool:
+    """ Identifies if a given video title consist only by capital letters.
+    The function compares if number of capital letters >= a give threshold
+    as defined by the @threshold parameter.
+    """
+    is_capitalized = False
+
+    only_letters_title = [l for l in video_title if l.isalpha()]
+    capital_letters = [l for l in only_letters_title if l.isupper()]
+
+    if (len(capital_letters) / len(list(only_letters_title))) >= threshold:
+        is_capitalized = True
+
+    return is_capitalized
+
