302bbf5077d616cae96abb9c62a530f90d920908
George Spyrou <gspyrou@umich.edu>
2022-03-27 10:47:36 +0100

    start api for youtube analyzer

--- tube/connection.py
+++ tube/connection.py
@@ -11,34 +11,41 @@ from bs4 import (BeautifulSoup,
 
 
 
-def url_to_bs4(target_url: str) -> BeautifulSoup:
-    """
-    Given a website link (URL), retrieve the corresponding website in an html
-    format.
-    Parameters
-    ----------
-    target_url : str
-        URL of the webpage that will be transformed to a BeautifulSoup object.
-    """
-    #print('Attempting to retrieve HTML object for {0}'.format(target_url))
-    request = urllib.request.urlopen(target_url)
-    if request.getcode() != 200:
-        raise Exception('Can not communicate with the client')        
-    else:
-        response = request.read()
-        response_html = BeautifulSoup(response, 'html.parser')
-        return response_html
+class YoutubeAnalyzer:
+    def __init__(self, video_url: str):
+        self.video_url = video_url
+        self.video_bsoup = self.url_to_bs4(video_url=video_url)
+ 
+    def url_to_bs4(self, video_url: str) -> BeautifulSoup:
+        """
+        Given a website link (URL), retrieve the corresponding website in an html
+        format.
+        Parameters
+        ----------
+        video_url : str
+            URL of the webpage that will be transformed to a BeautifulSoup object.
+        """
+        #print('Attempting to retrieve HTML object for {0}'.format(video_url))
+        request = urllib.request.urlopen(video_url)
+        if request.getcode() != 200:
+            raise Exception('Can not communicate with the client')        
+        else:
+            response = request.read()
+            response_html = BeautifulSoup(response, 'html.parser')
+            return response_html
+
+       
+    def _get_video_url(self) -> str:
+        return self.video_url
+    
+    def get_current_number_of_views(self, views_map={'itemprop': 'interactionCount'}) -> int:
+        number_of_views = self.video_bsoup.find_all('meta', attrs=views_map)[0].get('content')
+        return number_of_views
 
 
 
 
+t
+pryda_loving_you = YoutubeAnalyzer(video_url='https://youtu.be/iByQSaWTR1g')
 
-test = url_to_bs4(target_url='https://youtu.be/X6DIc_iQd9I')
-
-
-test.find_all("div")
-
-
-
-
-
+pryda_loving_you._get_video_url()
