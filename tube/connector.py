20128fb62cd242a97628450ee66b334951ddb188
George Spyrou <gspyrou@umich.edu>
2022-04-20 16:11:26 +0100

    create sample json file as catalog for video list

--- tube/connector.py
+++ tube/connector.py
@@ -4,23 +4,22 @@ Author: Georgios Spyrou (georgios.spyrou1@gmail.com")
 Package to collect metadata for Youtube Videos and apply formatting and
 transformations to prepare the data for further analysis.
 """
-
+import os
 import pandas as pd
 import json
 import sqlite3
 from tube.metadata import MetadataCollector
 from database.db import insert_into_q
 
-
 db_name = 'TubeDB.sqlite'
 metadata_table_name = 'TubeMetadata'
+config_filepath = os.path.join(os.getcwd(), 'config/video_catalog.json')
 
-#conn = sqlite3.connect(db_name)
-#c = conn.cursor()
+with open(config_filepath) as video_catalog:
+    catalog = json.load(video_catalog)
 
-video_url = 'https://youtu.be/yzTuBuRdAyA'
-video_url_2 = 'https://youtu.be/MOMthW49TU0'
-video_url_3 = 'https://youtu.be/NcXsK_u4ixI'
+video_url = catalog['videos'][0].get('url')
+video_url
 
 
 class TubeLogger(MetadataCollector):
@@ -71,20 +70,13 @@ class TubeLogger(MetadataCollector):
         conn.close()
 
 
-def MultiTubeWritter:
+class MultiTubeWritter:
     def __init_(self, video_collection):
         self.video_collection = video_collection
 
     def multivideo_meta_push_to_db() -> None:
         pass
 
+
 v1 = TubeLogger(video_url=video_url)
-v2 = TubeLogger(video_url=video_url_2)
-v3 = TubeLogger(video_url=video_url_3)
-
-v3.data
-
 v1.insert_into_metadata_table()
-v2.insert_into_metadata_table()
-v3.insert_into_metadata_table()
-
