0ad3dfb665ce60e0e70d6e7ed2d773d342997da9
George Spyrou <gspyrou@umich.edu>
2022-06-04 14:18:58 +0100

    add calculation for daily changes and rolling avgs

--- eda.py
+++ eda.py
@@ -39,8 +39,7 @@ data = data_header.merge(
     ).merge(
     data_channels,
     on='channel_id',
-    how='left',
-     suffixes=(None, "_channels")
+    how='left'
     )
 
 # Check if join works properly
@@ -49,13 +48,28 @@ assert (list(data_header['video_id'].unique()) == list(data_lines['video_id'].un
 
 
 # Drop Date columns from Header
-data = data.drop(columns=['CreatedDate_header', 'CreatedDatetime_header'])
+data = data.drop(columns=['CreatedDate_header', 'CreatedDatetime_header', 'CreatedDate', 'CreatedDatetime'])
 del data_lines
 del data_header
 del data_channels
 
 # Compute the ratio of likes/views per day
 data['target'] = data['number_of_likes'] / data['number_of_views']
+
+
+# Compute the daily change in views and likes
+data['likes_diff'] = data.groupby(['video_id'])['number_of_likes'].transform(
+    lambda x: x.diff(periods=1)
+    ) 
+
+data['views_diff'] = data.groupby(['video_id'])['number_of_views'].transform(
+    lambda x: x.diff(periods=1)
+    ) 
+
+
+# Computing 7-day rolling average
+data['seven_d_avg_likes_change'] = data['likes_diff'].rolling(7).mean()
+data['seven_d_avg_views_change'] = data['views_diff'].rolling(7).mean()
 
 
 def get_timeseries_for_video_id(
@@ -84,14 +98,29 @@ plot_date_change_for_col(
 plot_date_change_for_col(
     input_df=data, 
     col='number_of_views', 
-    date_col='CreatedDate', 
+    date_col='CreatedDate_lines', 
+    video_id='oTw8AECmUNA'
+    )
+
+plot_date_change_for_col(
+    input_df=data, 
+    col='likes_diff', 
+    date_col='CreatedDate_lines', 
+    video_id='oTw8AECmUNA'
+    )
+
+
+plot_date_change_for_col(
+    input_df=data, 
+    col='seven_d_avg_likes_change', 
+    date_col='CreatedDate_lines', 
     video_id='oTw8AECmUNA'
     )
 
 
 plot_change_in_views_and_likes(
     input_df=data, 
-    video_id='sPA3XIbho_A', 
+    video_id='3S1jrYq87Zw', 
     views_col='number_of_views', 
     likes_col='number_of_likes', 
     date_col='CreatedDate_lines'
